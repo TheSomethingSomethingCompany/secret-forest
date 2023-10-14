4ef67916df9955bb446416e7643c1a069ece9bd7
SatanshuMishra <satanshumishra@outlook.com>
2023-10-14 15:54:25 -0700

    🛠️ Further Developed UserAuthentication - Added styles for Sign-in Section - Added minimal styling to Sign-Up Section
    🧼️ Removed default Next.js styling from `global.css`.

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,7 +3,7 @@ import UserAuthentication from "./components/user-auth/UserAuthentication";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <UserAuthentication />
     </main>
   );
