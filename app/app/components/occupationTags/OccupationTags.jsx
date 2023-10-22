0a7df25c60703df387ca9b82567d442b664f393a
Shaheer <shaheershoaib11@gmail.com>
2023-10-22 08:43:45 -0700

    Allow tags to be removed

--- app/app/components/occupationTags/OccupationTags.jsx
+++ app/app/components/occupationTags/OccupationTags.jsx
@@ -1,26 +1,52 @@
-import {useState} from 'react'
-function OccupationTags(){
+import { useState } from 'react'
+function OccupationTags() {
+
     const [currentTags, setTags] = useState([]);
     const [inputValue, setInputValue] = useState('');
 
     const addTag = (event) => {
         if (event.key === 'Enter') {
-            if(!currentTags.includes(inputValue)){
-                setTags(prevTags => [...prevTags, inputValue]);
+            event.preventDefault();
+            if (!currentTags.includes(inputValue)) {
+                setTags(prevTags => {
+                const updatedTags = [...prevTags, inputValue];
+                console.log("inputValue:", inputValue);  // Debugging line
+                console.log("Updated Tags:", updatedTags);  // Debugging line
+                return updatedTags;
+                });
+                console.log("After:");
+                console.log(currentTags);
                 setInputValue('');
+                
             }
 
         }
     };
 
-    const onInputChange = function(event){
-        setInputValue(event.target.value);
-    }
+    const removeTag = function(event){
+        let tagValue = event.target.getAttribute("tag-association");
+        setTags( prevTags => prevTags.filter(tag => tag!==tagValue));
+        
+    };
 
-    return <div className = "flex">
-    <input type = "text" className = "text-black" onKeyDown = {addTag} id = "tag" value = {inputValue} onInput = {onInputChange}></input>
-    <div className = "bg-green-500 flex ml-5" id = "tagsContainer"> {currentTags.map( (tag) => <p key = {tag} className = "m-1">{tag}</p> )} </div>
-    </div>;
+    const onInputChange = function (event) { // Allows us to track most recent input chhange, so we can extract it when user hits enter key
+        setInputValue(event.target.value); // Any time the input in tag field changes, store that value in inputValue
+    };
+
+    return ( 
+        <div className="flex">
+            <input type="text" className="text-black" onKeyDown={addTag} value={inputValue} onInput={onInputChange}></input>
+            <div className="bg-green-500 flex ml-5 " id="tagsContainer">
+                { 
+                    currentTags.map((tag) => 
+                        <div key={tag} className="m-1 p-2 bg-blue-500 rounded-full flex justify-center items-center">
+                            <p>{tag}</p>
+                            <button tag-association={tag} onMouseDown = {removeTag} className = "ml-1 text-red-500">x</button>
+                        </div>)
+                }
+            </div>
+        </div>
+    );
 }
 
 export default OccupationTags;
