ef50ea47e23f18df283285cae82b1ba2b198945f
Shaheer <shaheershoaib11@gmail.com>
2023-12-01 23:08:37 -0800

    Implement Redirect To createProfile if no profile has been created

--- app/app/routes/CreateProfile.js
+++ app/app/routes/CreateProfile.js
@@ -5,36 +5,48 @@ const e = require("express");
 
 
 router.post('/api', async (req, res) => { // ./createAProfile/api will utilize this route. /api isn't necessary, but it makes it apparent that this is a route call.
-    const memberID = req.session.signUpMemberID; // Retrieve memberID from session data
-    console.log("[MEMBER ID]: " + memberID);
-    console.log("[REQUEST BODY FOR PROFILE CREATION]:\n" + req.body);
-    const {fullName, country, address, bio, occupationTags} = req.body;
-    try
+    const memberID = req.session.signUpMemberID; // Retrieve memberID from session data, which was set in the SignUp route
+    console.log("[MEMBER ID IN CREATE PROFILE]: " + memberID);
+    if(memberID == null) // If memberID is null, then a user is accessing this route without signing up first, since the memberID is stored in session data upon sign up
     {
-        if(fullName == "" || country == "" || address == "")
-        {
-            return res.json({ status: 422, message: 'Please enter all required fields' });
-        }
-        await db.none('INSERT INTO profile("memberID", "name", "country", "address", "bio") VALUES($1, $2, $3, $4, $5)', [memberID, fullName, country, address, bio]);
-        res.json({ status: 201, message: 'Profile created successfully' });
-        console.log("[PROFILE CREATION SUCCESSFUL FOR MEMBER ID]: " + memberID);
-        delete req.session.signUpMemberID // Destroy signUp session data
-        req.session.loggedInUserMemberID = memberID; // Store memberID in session data showing that the user is now logged in upon profile creation
-        req.session.save(); // Save session data
-    } 
+        res.json({ status: 401, message: 'Unauthorized access' });
+    }
+    else
+    { 
+        console.log("[MEMBER ID]: " + memberID);
+        console.log("[REQUEST BODY FOR PROFILE CREATION]:");
+        console.log(req.body);
+        const {fullName, country, address, bio, occupationTags} = req.body;
 
-    catch(error)
-    {
-        const errorResponse = {
-            data: null,
-            status: 500,
-            message: "Failed to create profile",
-            pgErrorObject: {
-            ...error,
-            },
-        };
-        console.log("[LOG RESPONSE]:\n" + JSON.stringify(errorResponse));
-        res.json({ status: 500, message: 'Failed to create profile' });
+        try
+        {
+            if(fullName == "" || country == "" || address == "")
+            {
+                res.json({ status: 422, message: 'Please enter all required fields' });
+            }
+            else
+            {
+                await db.none('INSERT INTO profile("memberID", "name", "country", "address", "bio") VALUES($1, $2, $3, $4, $5)', [memberID, fullName, country, address, bio]);
+                res.json({ status: 201, message: 'Profile created successfully' });
+                console.log("[PROFILE CREATION SUCCESSFUL FOR MEMBER ID]: " + memberID);
+                delete req.session.signUpMemberID // Destroy signUp session data
+                req.session.loggedInUserMemberID = memberID; // Store memberID in session data showing that the user is now logged in upon profile creation
+                req.session.save(); // Save session data
+            }
+        } 
+        
+        catch(error)
+        {
+            const errorResponse = {
+                status: 500,
+                message: "Failed to create profile",
+                pgErrorObject: {
+                ...error,
+                },
+            };
+            console.log("[LOG RESPONSE]:\n" + JSON.stringify(errorResponse));
+            res.json({ status: 500, message: 'Failed to create profile' });
+        }
     }
 });
 
