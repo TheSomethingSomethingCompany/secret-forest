aa8ce62a08ee00a6e49b7d2502d7666c1a8adf68
Shaheer <shaheershoaib11@gmail.com>
2024-01-22 04:44:45 -0800

    Optimize and change query to show all tags of a profile

--- app/app/routes/FetchSearchResults.js
+++ app/app/routes/FetchSearchResults.js
@@ -14,8 +14,18 @@ router.post("/api", async (req, res) => {
 
 
     if(occupationTags.length > 0){
-        query += `WHERE tag."tagName" = ANY($1) AND profile."name" LIKE $2`; // Even if name is empty, it will still work because empty string is a substring of all strings
-        query += `GROUP BY profile."memberID", profile."name", profile."country"`;
+        query = `SELECT profile."memberID", profile."name", profile."country", array_agg(tag."tagName") as "tags" 
+        FROM profile JOIN (
+          select "memberID"
+          from user_tag join tag on user_tag."tagID" = tag."tagID"
+          where tag."tagName" = any($1) 
+        ) as matchingProfiles ON profile."memberID" = matchingProfiles."memberID"
+        JOIN user_tag ON profile."memberID" = user_tag."memberID"
+        join tag on user_tag."tagID" = tag."tagID"
+        group by profile."memberID", profile."name", profile."country"
+        having profile.name like $2`;
+
+      
         const profiles = await db.any(query, [occupationTags, name+'%']);
         console.log("[SUCCESS]: PROFILE FETCHED SUCCESSFUL");
         res.json({
@@ -29,8 +39,13 @@ router.post("/api", async (req, res) => {
 
         }
     else if(occupationTags.length == 0){ // If no tags are selected, then just search by name. even if name is empty, it will still work because empty string is a substring of all strings
-        query += `WHERE profile."name" LIKE $1`;
-        query += `GROUP BY profile."memberID", profile."name", profile."country"`;
+        query = `SELECT profile."memberID", profile."name", profile."country", array_agg(tag."tagName") as "tags" 
+        FROM profile 
+        JOIN user_tag ON profile."memberID" = user_tag."memberID"
+        join tag on user_tag."tagID" = tag."tagID"
+        group by profile."memberID", profile."name", profile."country"
+        having profile.name like $1;`;
+
         const profiles = await db.any(query, [name + '%']);
         console.log("[SUCCESS]: PROFILE FETCHED SUCCESSFUL");
         res.json({
@@ -40,17 +55,7 @@ router.post("/api", async (req, res) => {
             pgErrorObject: null,
           });
         }
-    else{
-        query += `GROUP BY profile."memberID", profile."name", profile."country"`;
-        const profiles = await db.any(query);
-        console.log("[SUCCESS]: PROFILE FETCHED SUCCESSFUL");
-        res.json({
-            data: profiles,
-            status: 202,
-            message: "Search Query Fetch Successful",
-            pgErrorObject: null,
-          });
-    }
+   
 
    
 
