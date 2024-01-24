61ee06893fbe2a11c4514fc1e86c0412a05f60d0
Satanshu Mishra <satanshumishra@outlook.com>
2024-01-23 20:06:41 -0800

    Created Search Bar for searching through Conversations
    - Implemented Responsive Search Bar.
    - Currently only front-end present. No back-end functionality.
    - Added Keyboard shortcut to focus the input field. The shortcut used is `k`.

--- app/app/sandbox/page.tsx
+++ app/app/sandbox/page.tsx
@@ -1,10 +1,35 @@
+"use client";
 import React from "react";
 import PenguinC from "@/app/images/PenguinC.jpeg";
+import { useEffect, useRef } from "react";
 
 function page() {
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        const handleKeyDown = (event:any) => {
+			event.preventDefault();
+            if (event.code === 'KeyK') { 
+                inputRef.current?.focus();
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
   return (
 	<div className="flex flex-row w-screen chat-height justify-center items-start px-8 py-4">
 		<section className="rounded-lg shadow w-[30rem] min-w-[30rem] max-w-[30rem] h-full mr-2">
+			<div className="flex flex-row outline-black shadow rounded-lg h-fit w-full px-4 py-2 m-4 max-w-[28rem] items-center justify-center">
+				<i className="ri-search-eye-fill text-[1.7rem] mr-1"></i>
+				<form className="flex-1 h-auto ml-1">
+					<input ref={inputRef} className="w-full h-full px-2 outline-none" placeholder="Search for user"/>
+				</form>
+				<kbd>k</kbd>
+			</div>
 			<div className="flex flex-row justify-between items-center p-4 border-2 border-transparent rounded-lg h-full max-h-[8rem] m-4 shadow">
 				<div className="flex flex-row justify-start items-center mr-1 max-w-[25rem] overflow-hidden">
 					<img
