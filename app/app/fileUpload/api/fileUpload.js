761544f99ab0df0502fcb3365412e32a677fddbc
AhmedAlRubaii <ahmedhamahmood@hotmail.com>
2024-02-01 06:42:55 +0400

    added frontend page to test the file upload to s3

---
+++ app/app/fileUpload/api/fileUpload.js
@@ -0,0 +1,18 @@
+//DUMMY CODE DUMMY CODE DUMMY CODE DUMMY CODE DUMMY CODE DUMMY CODE DUMMY CODE DUMMY CODE
+
+export async function createAProfile(data){
+    const response = await fetch('http://localhost:6969/createAProfile/api', {
+        method: 'POST',
+        body: data,
+        credentials: "include",
+    });
+
+    let resBody = await response.json(); // Retrieve response body and turn into JSON object
+    console.log("[RESPONSE BODY CREATE PROFILE]:") 
+    console.log(resBody);
+    return resBody;
+   }
+
+   export default createAProfile;
+
+//DUMMY CODE DUMMY CODE DUMMY CODE DUMMY CODE DUMMY CODE DUMMY CODE DUMMY CODE DUMMY CODE
---
+++ app/app/fileUpload/page.tsx
@@ -0,0 +1,57 @@
+"use client"
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useState } from "react";
+import fileUpload from "./api/fileUpload.js";
+
+
+
+export default function main(){
+
+  const [file, setFile] = useState(null);
+
+  const fileSelected = e => {
+    const file = e.target.files[0]
+		setFile(file)
+    console.log(file)
+	}
+
+
+  async function submit(e){
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", file);
+    
+    var response = await fileUpload(formData)
+    if(response.status == 200){
+      alert("file uploaded successfuly!");
+    }else{
+      alert("no response from server");
+    }
+  }
+
+return(
+    <>
+
+    <div>
+      <form onSubmit={submit}>
+        <input type="text" className="border"/>
+        <input type="file"id="file" onChange={fileSelected}/>
+        <button type="submit">Click here to upload your file</button>
+      </form>
+    </div>    
+    
+    </>
+
+
+
+)
+
+
+
+
+}
+
+
+
