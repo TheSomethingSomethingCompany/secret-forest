1613cbf9db12fb06f8b894c9361ef4c68289e32a
Satanshu Mishra <satanshumishra@outlook.com>
2024-04-03 14:55:33 -0700

    Database Switch
    - Changed Database from local Postgres to Supabase.
    - Removed Postgres image from docker-compose.
    - Updated details in db-connection.js.

--- app/app/db-connection.js
+++ app/app/db-connection.js
@@ -1,15 +1,11 @@
-// db.js
-
-// write `const db=require('../db-connection.js') in every route file
-
 const pgp = require('pg-promise')();
 
 const cn = {
-    host: 'db', // the service name from docker-compose.yml
-    port: 5432,
+    host: 'aws-0-ca-central-1.pooler.supabase.com',
+	port: 5432,
     database: 'postgres', 
-    user: 'postgres',
-    password: 'postgres'
+    user: 'postgres.tmeseckyonsoxkmspewp',
+    password: 'postgres@499'
 };
 
 const db = pgp(cn);
