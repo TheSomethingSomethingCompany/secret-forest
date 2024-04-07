d8bc72a3ae0010f13dcafe97c096561e17874d4d
Satanshu Mishra <satanshumishra@outlook.com>
2024-04-07 03:54:59 -0700

    Tailwind Config Update &  Search UI
    - Updated Tailwind config to apply styles to app file types for Next.js.
    - Updated UI for Search Page.

--- app/app/components/searchBar/Searchbar.jsx
+++ app/app/components/searchBar/Searchbar.jsx
@@ -2,10 +2,10 @@
 import { useState } from "react";
 import OccupationTags from "../occupationTags/OccupationTags";
 
-function SearchBar({ searchQ, setSearchQ, searchBy, setSearchBy, onSearch }) {
+function SearchBar({ searchQ, setSearchQ, searchBy, setSearchBy, onSearch, placeholder = "Search for Requests..." }) {
 	return (
-		<div className="flex-1 flex flex-col lg:flex-row w-full my-2">
-			<div className="flex flex-row justify-between items-center pr-4 px-4 bg-[#1F2932] rounded-lg my-2 lg:my-0 lg:mr-2">
+		<div className="flex-1 flex flex-col lg:flex-row w-full my-2 bg-transparent">
+			<div className="flex flex-row justify-between items-center py-4 px-4 bg-[#1F2932] rounded-lg my-2 lg:my-0 lg:mr-2">
 				<p className="font-semibold text-white py-2 lg:py-0 px-4">Search By:</p>
 				<select
 					className="bg-[#1F2932] text-white hover:text-white"
@@ -30,7 +30,7 @@ function SearchBar({ searchQ, setSearchQ, searchBy, setSearchBy, onSearch }) {
 					type="search"
 					value={searchQ || ""}
 					onChange={(event) => setSearchQ(event.target.value)}
-					placeholder="Search for Requests..."
+					placeholder={placeholder}
 					className="text-white px-2 outline-none focus:outline-none bg-[#1F2932] flex-1"
 				/>
 				<button type="submit">
